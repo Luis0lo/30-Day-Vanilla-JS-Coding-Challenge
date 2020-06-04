984176b0dcc231d95e434179c4edbd72baf76095
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2020-06-04 18:04:00 +0100

    clock

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date(); //Thu Jun 04 2020 17:32:42 GMT+0100 (British Summer Time)
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90; //trasnform seconds into degrees
+  //+90 represents degrees, as we set on stylesheet
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
